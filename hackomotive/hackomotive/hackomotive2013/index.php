f23ca64dd28cdba4b7384475dd597a6d4003f7ba
froglozoid <npotiyenok@npotiye-mbp113.edmunds.hq>
2014-03-15 13:57:28 -0700

    Hackomotive
    Edmunds.com hackomotive website files

---
+++ hackomotive/hackomotive/hackomotive2013/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en-us" xmlns:og="http://ogp.me/ns#">
+<head>
+	<meta charset="utf-8">
+	<title>Hackomotive 2013: Reinventing The Automotive Shopping Experience</title>
+	<meta property="og:title" content="Hackomotive 2013: Reinventing The Automotive Shopping Experience"/>
+	<meta property="og:image" content="http://www.hackomotive.com/media/logo_sq.jpg"/>
+	<meta property="og:url" content="http://www.hackomotive.com/"/>
+	<meta property="og:type" content="activity"/>
+	<meta property="og:site_name" content="Hackomotive"/>
+	<meta property="og:description" content="Hackomotive is a two-day challenge that focuses on re-imagining the car shopping experience by bringing together consumers, dealers, manufacturers, designers and technologists to shatter the status quo. In providing this creative, collaborative platform for groups of intellectually diverse thinkers, our goal is to generate a surge of bold and innovative new ideas. A panel of judges will then evaluate the proposed solutions to determine the winners."/>
+	<meta property="og:latitude" content="34.0299171"/>
+	<meta property="og:longitude" content="-118.4705554"/>
+	<meta property="og:street-address" content="1620 26th Street"/>
+	<meta property="og:locality" content="Santa Monica"/>
+	<meta property="og:region" content="CA"/>
+	<meta property="og:postal-code" content="90404"/>
+	<meta property="og:country-name" content="USA"/>
+	<meta property="og:email" content="ielshareef@edmunds.com"/>
+	<meta property="og:phone_number" content="310-309-3013"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+	<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/global_results.css" rel="stylesheet">
+	<meta name="description" content="">
+    <meta name="author" content="">
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="overview-container">
+		<div class="container">
+			<div class="twitter-follow"><a href="https://twitter.com/Hackomotive" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @hackomotive</a></div>
+			<h1>HACK-O-MOTIVE<span>2013</span></h1>
+			<p class="tagline">Reinventing the automotive shopping experience</p>
+			<div class="navbar">
+				<div class="navbar-inner">
+					<div class="container">
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						<a class="brand" href="#">Overview</a>
+						<div class="nav-collapse collapse">
+							<ul class="nav nav-pills">
+								<li class="active"><a href="index.php">Overview</a></li>
+								<li><a href="schedule.html">Schedule</a></li>
+								<li><a href="judges.html">Judges</a></li>
+								<li><a href="results.html">Results</a></li>
+								<li><a href="media.html">Photos/Videos</a></li>
+								<li><a href="faq.html">FAQs</a></li>
+								<li><a href="buzz.html">Buzz</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span12 carsbg">
+					<div class="row-fluid">
+						<div class="span4 offset1">
+							<div>
+							FEBRUARY 26 – FEBRUARY 28, 2013<br />
+							SANTA MONICA, CA
+							</div>
+							<address>
+							<strong>QUESTIONS?</strong><br />
+							Please contact us at <a href="mailto:hackomotive@edmunds.com">hackomotive@edmunds.com</a>
+							</address>
+						</div>
+						<div class="span6">
+							<div>
+							<p>Hackomotive is a two-day challenge that focuses on re-imagining the car shopping experience by bringing together consumers, dealers, manufacturers, designers and technologists to shatter the status quo. Car buying is the second biggest purchase behind a home for most consumers and remains an unpleasant, anxiety-filled experience that has changed little even with the benefit of online resources. By bringing together the right mix of thought leaders and change agents, we believe we can reinvent the car buying experience from the ground up. A panel of judges will then evaluate the proposed solutions to determine the winners.</p>
+							<p><iframe width="530" height="298" src="http://www.youtube.com/embed/BcRENHGwbTE" frameborder="0" allowfullscreen></iframe></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+	
+	<div id="footer">
+		<div class="container"
+		  <div class="row-fluid">
+			<div class="span5 offset1">&copy; Edmunds.com, Inc.</div>
+			<div class="byline">Presented by <a href="http://www.edmunds.com/" target="_blank"><span class="edmunds-logo">Edmunds.com</span></a></div>
+		  </div>
+	  </div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</body>
+</html>
